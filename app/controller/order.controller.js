d12e6e1e18fdee53c0b97c91d9525d5050b6453a
SudiptoSarkar123 <sudiptosarkar833@gmail.com>
2025-10-22 18:19:30 +0530

    add order list

--- app/controller/order.controller.js
+++ app/controller/order.controller.js
@@ -104,10 +104,64 @@ export const createOrder = asynchandler(async (req, res) => {
   });
 });
 
-export const getAllOrders = asynchandler(async (req, res) => {
-  const orders = await Order.find().populate("products.productId");
-  res.status(200).json({
+export const getMyOrders = asynchandler(async (req, res) => {
+  const userId = req.user.id;
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  // Get paginated orders
+  const orders = await Order.find({ user: userId })
+    .populate("products.productId", "name price imageUrl")
+    .skip(skip)
+    .limit(limit)
+    .lean(); 
+
+    // console.log(orders[0].products)
+  const totalOrders = await Order.countDocuments({ user: userId });
+
+  if (!orders.length) throw createError(404, "Orders not found");
+
+  // Format each order
+  const formattedOrders = orders.map(order => {
+    let totalAmount = 0;
+    let totalQuantity = 0;
+
+    const products = order.products.map(item => {
+      totalAmount += item.productId.price * item.quentity;
+      totalQuantity += item.quentity;
+
+      return {
+        _id: item.productId._id,  
+        name: item.productId.name,
+        price: item.productId.price,
+        image: item.productId.imageUrl,
+        quentity: item.quentity
+      };
+    });
+
+    return {
+      orderId: order._id,
+      totalAmount,
+      totalQuantity,
+      products,
+      createdAt: order.createdAt
+    };
+  });
+
+  return res.status(200).json({
     status: true,
-    orders,
+    page,
+    limit,
+    totalOrders,
+    totalPages: Math.ceil(totalOrders / limit),
+    orders: formattedOrders
   });
 });
+
+
+
+export const getOrderDetails = asynchandler(async (req, res) => {
+  const orderId = req.params.id;
+})
--- app/router/order.route.js
+++ app/router/order.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import authCheck from "../middleware/authMiddleware.js";
-import { createOrder } from "../controller/order.controller.js";
-import upload from "../middleware/multerMiddleware.js";
+import { createOrder, getMyOrders } from "../controller/order.controller.js";
+
 const OrderRouter = express.Router();
 
 // This is order route
 OrderRouter.post("/orderFromCart", authCheck, createOrder);
+OrderRouter.get("/", authCheck, getMyOrders);
 
 export default OrderRouter;
